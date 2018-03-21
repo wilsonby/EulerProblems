b725f72772fd8aa0c379c232b88f6410d1728e28
Byron Wilson <byron.wilson@live.com>
2018-03-21 14:42:59 -0700

    Cleaned Solution

--- Project32.py
+++ Project32.py
@@ -5,20 +5,29 @@ Created on Wed Mar 21 13:31:27 2018
 @author: byron
 """
 import itertools
-Z = range(1,10)
-pandigitalProduct = set()
-for p in itertools.permutations(range(1,10)):
-    P1 = (p[0]*10 +p[1]) * (100*p[2] + 10*p[3] + p[4])
-    P2 = 1000*p[5] + 100*p[6] + 10*p[7] + p[8]
-    if P1 == P2:
-        print ((p[0]*10 +p[1]),(100*p[2] + 10*p[3] + p[4]), P2)
-        pandigitalProduct.add(P2)
+def concatNum(T):
+    '''given a tuple T of single digit intergers, return the concatination of 
+    the tuple as an interger'''
+    S = 0
+    for i in range(len(T)):
+        S += T[i]*10**(len(T)-i-1)
+    return S
+
+if __name__ == '__main__':
+    pandigitalProduct = set()
     
-    P1 = p[0]*(1000*p[1] +100*p[2] + 10*p[3] + p[4])
-    P2 = 1000*p[5] + 100*p[6] + 10*p[7] + p[8]
-    if P1 == P2:
-        print (p[0],(1000*p[1] +100*p[2] + 10*p[3] + p[4]), P2)
-        pandigitalProduct.add(P2)
+    for p in itertools.permutations(range(1,10)):
         
-print pandigitalProduct
-print sum(pandigitalProduct)
+        P1 = concatNum(p[0:2]) * concatNum(p[2:5])
+        P2 = concatNum(p[5:])
+        
+        if P1 == P2:
+            pandigitalProduct.add(P2)
+        
+        P1 = p[0] * concatNum(p[1:5])
+
+        if P1 == P2:
+            pandigitalProduct.add(P2)
+            
+
+    print sum(pandigitalProduct)
